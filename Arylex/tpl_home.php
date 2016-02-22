0eb928105a33edc43aee9ff0704d9d1bc06fb206
pmcruzm <pmcruzm@Imac-de-Pedro.local>
2016-02-22 21:41:50 +0000

    Cambios home v1 22-2-2016

--- Arylex/tpl_home.php
+++ Arylex/tpl_home.php
@@ -7,22 +7,34 @@
  */
 ?>
 <?php get_header(); ?>
+	<?php
+      if ( have_posts() ) : while ( have_posts() ) : the_post();
+		$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail_size' );
+		$cover_top = $thumb['0'];
+	?>
+	<div id="cover_top" style="background-image:url(<?php echo $cover_top;?>)">
+    	<p><?php the_content();?></p>
+        <!--Info del player-->
+        <?php
+        	
+		?>
+    </div>
 	<div class="box-main">
     	<div class="left_box">
-            <h3><?php echo types_render_field("title-top-box-home",array("output"=>"html"))?></h3>
-            <p><?php echo types_render_field("content-top-box-home",array("output"=>"html"))?></p>
+            <h3><?php echo types_render_field("title-top-box-home",array("output"=>"html"));?></h3>
+            <p><?php echo types_render_field("content-top-box-home",array("output"=>"html"));?></p>
         </div>
         <div class="right_box">
-        	<?php echo types_render_field("image-top-box-home",array("output"=>"normal","alt"=>"Texto alternativo"))?>
+        	<?php echo types_render_field("image-top-box-home",array("output"=>"normal","alt"=>__('Texto alternativo','arylex')));?>
         </div>
     </div>
     <div class="box-main">
     	<div class="left_box">
-         	<img src="<?php echo types_render_field("image-bottom-home-box",array("output"=>"raw"))?>">   
+         	<img src="<?php echo types_render_field("image-bottom-home-box",array("output"=>"normal","alt"=>__('Texto alternativo','arylex')));?>">   
         </div>
         <div class="right_box">
-        	<h3><?php echo types_render_field("title-bottom-home-box", array("output"=>"html"))?></h3>
-            <p><?php echo types_render_field("content-bottom-home-box", array("output"=>"html"))?></p>	
+        	<h3><?php echo types_render_field("title-bottom-home-box", array("output"=>"html"));?></h3>
+            <p><?php echo types_render_field("content-bottom-home-box", array("output"=>"html"));?></p>	
         </div>
     </div>
     <!--Banner Products-->
@@ -35,9 +47,13 @@
     <div class="box_KB">
     	<div class="left_box">
         	<!--Menús Key Benefits-->
-            <h3></h3>
+            <h3><?php _e('KEY BENEFITS','arylex' )?></h3>
         </div>
         <div class="right_box">
+        	
         </div>
     </div>
+    <?php
+       endwhile; endif;wp_reset_query();
+	?>
 <?php get_footer(); ?>
