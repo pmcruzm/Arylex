9ebf801b33f9e8791df20fca53b84f62413d893d
pmcruzm <pmcruzm@Imac-de-Pedro.local>
2016-02-29 16:13:58 +0000

    Cambios FAQ

--- Arylex/alm_templates/default.php
+++ Arylex/alm_templates/default.php
@@ -2,5 +2,5 @@
    <?php if ( has_post_thumbnail() ) { the_post_thumbnail(array(150,150));
    }?>
    <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-   <?php the_excerpt(); ?>
+   <?php the_content(); ?>
 </li>
