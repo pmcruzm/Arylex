15fd51a9ae427d96dccc4958c073e1032948cba2
pmcruzm <pmcruzm@Imac-de-Pedro.local>
2016-03-15 21:28:49 +0000

    Template products

---
+++ Arylex/tpl_products.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @template name: Products
+ * @package WordPress
+ * @subpackage Arylex_theme
+ * @since Arylex Theme 1.0
+ */
+?>
+<?php get_header(); ?>
+<?php
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+	$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail_size' );
+	$cover_top = $thumb['0'];
+?>
+<header class="header-img" style="background-image:url(<?php echo $cover_top;?>)">
+    <span class="furrows"></span>
+    <div class="highlight">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h2><?php the_title();?></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<main id="main" role="main">
+
+    <article class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <?php the_content();?>
+                <p><img src="<?php bloginfo('template_url'); ?>/img/logo-grey.png" class="img-responsive"></p>
+            </div>
+            <div class="col-md-6 col-md-offset-1">
+                <img src="<?php echo types_render_field("image-products",array("output"=>"raw"));?>" class="img-responsive center-block">
+            </div>
+        </div>
+    </article>
+
+</main>
+<?php
+    endwhile; endif;wp_reset_query();
+?>
+<?php get_footer(); ?>
